2d0f54e07bd081c6efe1fabe4732f372e4ee6630
bhavy11 <89129263+bhavy11@users.noreply.github.com>
2025-04-10 08:00:01 +0530

    Add files via upload

---
+++ Assignment 1/Task 1.py
@@ -0,0 +1,10 @@
+a=float(input('input first number: '))
+b=float(input('input second number: '))
+add=a+b
+sub=a-b
+multi=a*b
+divi=a/b
+print('Addition: ',add)
+print('subtraction: ',sub)
+print('Multiplication: ',multi)
+print('Division: ',divi)
---
+++ Assignment 1/Task 2.py
@@ -0,0 +1,3 @@
+a=str(input('input first name: '))
+b=str(input('input last name: '))
+print("Hello, " + a +' '+ b +"! Welcome to the python program")
